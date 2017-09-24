2836becf21927910019797f8569e46662834a076
yedupaloor <32161418+yedupaloor@users.noreply.github.com>
2017-09-24 16:23:01 +0530

    final

---
+++ Publish/web/scripts/Hotel.js
@@ -0,0 +1,29 @@
+﻿(function () {
+    var Hotel = function ($http, $location) {
+        var url = "http://localhost:4383/api/Hotels/";
+
+        var getHotels = function () {
+            return $http.get(url)
+                .then(function (response) {
+                    return response.data;
+                });
+        };
+
+        var getHotel = function (id) {
+            url += id;
+            return $http.get(url)
+                .then(function (response) {
+                    return response.data;
+                });
+        };
+
+        return {
+            getHotels: getHotels,
+            getHotel: getHotel
+        };
+
+    };
+    var module = angular.module("Hotels");
+    module.factory("Hotel", Hotel);
+
+}());
---
+++ Publish/web/scripts/HotelsController.js
@@ -0,0 +1,20 @@
+﻿(function () {
+    var module = angular.module("Hotels");
+
+    var HotelsController = function ($scope, Hotel, $location) {
+        var onLoad = function (data) {
+            $scope.hotels = data;
+        }
+        var onError = function (reason) {
+            $scope.error = "Something went wrong!";
+        }
+        Hotel.getHotels().then(onLoad, onError);
+
+        $scope.showImages = function (id) {
+            $location.path("/hotels/" + id);
+        }
+    };
+
+    module.controller("HotelsController", ["$scope", "Hotel", "$location", HotelsController]);
+
+}());
